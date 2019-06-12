ffcd2325193243113fbba3252841d8dd1eacfc44
wherestheLamsauce <33526834+wherestheLamsauce@users.noreply.github.com>
2019-06-11 22:07:06 -0700

    Update test_functions.py

--- MyProjectFolder/my_module/test_functions.py
+++ MyProjectFolder/my_module/test_functions.py
@@ -2,7 +2,7 @@ from functions import *
 
 def test_check_college_input():
     test = CollegeProcess()
-    test.check_college_input('Killmonger') 
-    assert 'Killmonger' in test.college_choices
+    test.check_college_input("Killmonger") 
+    assert "Killmonger" in test.college_choices
 
     
