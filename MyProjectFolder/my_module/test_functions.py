361b279b118447589eeb5c3f63a8fedfeec71e29
wherestheLamsauce <33526834+wherestheLamsauce@users.noreply.github.com>
2019-06-11 22:04:21 -0700

    Update test_functions.py

--- MyProjectFolder/my_module/test_functions.py
+++ MyProjectFolder/my_module/test_functions.py
@@ -1,6 +1,6 @@
 from functions import *
 
-def test_CheckCollegeInput():
+def test_check_college_input():
     test = CollegeProcess()
     test.check_college_input('Killmonger') 
     assert 'Killmonger' in test.college_choices
